0cb6454d1579e4e62d87e8ea4772ce6d2ca88ac8
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2025-10-05 16:20:54 +0300

    Update index.ts

--- projects/frontend/src/lib/base/api/list/VRChat/index.ts
+++ projects/frontend/src/lib/base/api/list/VRChat/index.ts
@@ -30,7 +30,7 @@ export class VRChat {
   async init() {
     if (this._init) return;
     this._init = true;
-    this.userAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0 VRCX/${await this._fetchVersion()} https://github.com/vrcx-team/VRCX`;
+    this.userAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0 VRCX/${await this._fetchVersion()} https://github.com/vrcx-team/VRCX`;
     await this.users.init();
     await this.pipeline.init();
   }
