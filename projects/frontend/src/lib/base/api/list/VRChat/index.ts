69acfc941d48612395f40a136fbe78b619407cc1
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2025-10-05 15:58:52 +0300

    a

--- projects/frontend/src/lib/base/api/list/VRChat/index.ts
+++ projects/frontend/src/lib/base/api/list/VRChat/index.ts
@@ -11,20 +11,33 @@ export class VRChat {
   users = new Users(this);
   avatars = new Avatars(this);
   pipeline = new Pipeline(this);
+  _version: string | null = null;
   _init = false;
   constructor(public api: VRCKitAPI) {
 
   }
 
+  async _fetchVersion() {
+    if (this._version) return this._version;
+    const res = await this.api.http.fetch({
+      side: "Server",
+      url: "https://raw.githubusercontent.com/vrcx-team/VRCX/refs/heads/master/Version"
+    });
+    this._version = res.data.trim();
+    return this._version;
+  }
+
   async init() {
     if (this._init) return;
     this._init = true;
-    this.userAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0 VRCKit/${this.api.constants.WebVersion} https://vrckit.com/`;
+    this.userAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0 VRCX/${await this._fetchVersion()} https://github.com/vrcx-team/VRCX`;
     await this.users.init();
     await this.pipeline.init();
   }
 
   async destroy() {
+    this._init = false;
+    this._version = null;
     this.userAgent = null;
     await this.users.destroy();
     await this.pipeline.destroy();
